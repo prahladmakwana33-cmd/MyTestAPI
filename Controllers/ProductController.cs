b066edb027b69af20ca0f7e8f90f40b14eb97a9d
PRAHLAD\prahl <prahlad.makwana@epistic.in>
2026-04-04 20:17:15 +0530

    Fix

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -4,16 +4,16 @@ namespace MyTestAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class ProductController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<ProductController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public ProductController(ILogger<ProductController> logger)
         {
             _logger = logger;
         }
