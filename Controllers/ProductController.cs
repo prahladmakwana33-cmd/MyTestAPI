72c6c40e57865f0de6eeb4878a36924a9cfaed92
PRAHLAD\prahl <prahlad.makwana@epistic.in>
2026-04-05 16:58:34 +0530

    Change Controller Product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -4,16 +4,16 @@ namespace MyTestAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class TestController : ControllerBase
+    public class ProductController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<TestController> _logger;
+        private readonly ILogger<ProductController> _logger;
 
-        public TestController(ILogger<TestController> logger)
+        public ProductController(ILogger<ProductController> logger)
         {
             _logger = logger;
         }
